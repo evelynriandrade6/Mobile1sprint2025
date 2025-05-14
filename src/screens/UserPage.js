0b2b8f123f0168e750d7007af2c43a0ea25150e3
joaopedroalexan <joaopedroalexsilva@gmail.com>
2025-05-14 16:18:41 -0300

    14/05/2025

---
+++ src/screens/UserPage.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  StyleSheet,
+  Image,
+} from "react-native";
+import api from "../axios/axios";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+ 
+export default function userPage() {
+    
+}
